7235a58591abbad2cc53354d0e0954b990183819
yoshiddddd <yoshidakerapo@gmail.com>
2024-08-22 22:19:13 +0900

    init

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+//うに
+func calculate_number(number int) int {
+	fmt.Println("Calculating...")
+
+	//ここに計算処理を書く
+	return number
+}	
+
+//にっしー
+func check_number(number int) bool{
+	//もしnumberが正ならtrueを返す
+}
+
+
+func main() {
+	var number int
+	fmt.Println("Hello, World!")
+	fmt.Scan(&number)
+
+	if check_number(number) {
+	number =calculate_number(number)
+	}
+	else{
+		fmt.Println("Please input positive number")
+	}
+}
